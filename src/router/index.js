53ab4da2517d564b9d2cdc7331aaac152d887c6f
Kevin agudelo <kevinagude25@gmail.com>
2023-11-18 17:20:33 -0500

    Actualización

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,34 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Comunas from '../views/Comunas';
+import NewComuna from '../components/comunas/NewComuna.vue';
+import Home from '../views/Home.vue';
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Home
   },
+
+{
+  path: '/comunas',
+  name: 'Comunas',
+  component: Comunas
+},
+
+{
+  path: '/editar-comuna/:id',
+  name: 'EditarComuna',
+  component: EditarComuna
+},
+
+{
+  path: '/add-comuna/',
+  name: 'NewComuna',
+  component: NewComuna
+},
+
   {
     path: '/about',
     name: 'about',
